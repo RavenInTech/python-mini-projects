759696a254d20a0b7d499f93c7d45f2f87faba73
Raven <126422775+RavenInTech@users.noreply.github.com>
2023-05-08 14:17:40 -0400

    Add user name generator project

---
+++ user-name-generator/main.py
@@ -0,0 +1,10 @@
+print("Welcome to the User Name Generator.")
+
+favorite_food = input("What is your favorite food?\n")
+favorite_color = input("What is your favorite color?\n")
+favorite_number = input("What is your favorite number?\n")
+
+user_name = favorite_food + favorite_color + favorite_number
+
+print ("Your username will be: " + user_name.lower())
+
